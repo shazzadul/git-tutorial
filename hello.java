fc47bf4f4c17197c167e30e846cdcae2e90d642c
shazzadul <shazzad007@yahoo.com>
2019-08-15 23:38:09 +0600

    Add a new method called display hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,11 @@
 public class hello {
     public static void main(string[] args){
         System.out.printIn("Vs Code is Awesome...")
+
+        greeting ("Shazzadul")
+    }
+
+    publick static void greeting(string name){
+        System.out.printIn("Welcome" + name);
     }
 }
